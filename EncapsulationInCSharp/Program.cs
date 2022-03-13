7c801b854b88899c8dc96e336a6a8012dc8dd359
Usman-Kashmiri <usmankashmiri378@gmail.com>
2022-03-13 22:28:05 +0500

    Applied Validation on user input in console

--- EncapsulationInCSharp/Program.cs
+++ EncapsulationInCSharp/Program.cs
@@ -9,22 +9,44 @@ namespace EncapsulationInCSharp
 
         public void setEmpId(int id)
         {
-            this.emp_id = id;
+            if (emp_id >= 0)
+            {
+                this.emp_id = id;
+            } else
+            {
+                Console.WriteLine("Employee id cannot be a negative number...!");
+            }
         }
 
         public void getEmpId()
         {
-            Console.WriteLine("Emplpoyee id is: {0}",this.emp_id);
+            if (emp_id >= 0)
+            {
+                Console.WriteLine("Emplpoyee id is: {0}", this.emp_id);
+            }
         }
 
         public void setEmpName(string Name)
         {
-            this.emp_name = Name;
+            if (string.IsNullOrEmpty(Name) == true)
+            {
+                Console.WriteLine("Employee name is required...!");
+            } else
+            {
+                this.emp_name = Name;
+            }
         }
 
         public void getEmpName()
         {
-            Console.WriteLine("Emplpoyee name is: {0}", this.emp_name);
+            if (string.IsNullOrEmpty(emp_name) == true)
+            {
+               // Console.WriteLine("Employee name is required...!");
+            }
+            else
+            {
+                Console.WriteLine("Emplpoyee name is: {0}", this.emp_name);
+            }
         }
     }
 
@@ -33,9 +55,11 @@ namespace EncapsulationInCSharp
         static void Main(string[] args)
         {
             employee empobj = new employee();
-            empobj.setEmpId(1);
+            Console.WriteLine("Please enter employee id:");
+            empobj.setEmpId(int.Parse(Console.ReadLine()));
+            Console.WriteLine("Please enter employee name:");
+            empobj.setEmpName(Console.ReadLine());
             empobj.getEmpId();
-            empobj.setEmpName("Kanshif");
             empobj.getEmpName();
             Console.ReadLine();
         }
